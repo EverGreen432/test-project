b430ea4cba43ee4061e7521b4b913306b8325be7
EverGreen432 <gavayka3@gmail.com>
2024-12-01 10:27:02 +0600

    создали новый файл питона
    тема:
    1-словарь
    2-список
    3-кортеж

---
+++ index.py
@@ -0,0 +1,39 @@
+fruits=["banan", "apple" , "orange"]#список 
+print(fruits[1])
+colors=("red", "green" ,"white")#кортеж
+print(colors)
+students={
+    "alice": 16,
+    "siri" : 10,
+     "oleg": 44
+}
+print(students)
+students["oleg"] = 20
+print("ему ",students["oleg"], "лет")
+del students["oleg"]
+print(students)
+# название-словаря={
+#     ключ:значение
+#     ключ:значение
+# }
+fruit={
+    "apple":"red",
+    "banan":"yellow",
+    "orange":"orange",
+     "pineapple":"darkyellow"
+}
+print(fruit)
+fruit["pineapple"]="darkgreen"
+print(fruit)
+del fruit["pineapple"]
+print(fruit)
+bedtnumbers={
+    5:"great",
+    13:"something will happen"
+}
+print(bedtnumbers[13])
+haha={
+    True:"its true",
+    False:"its false"
+}
+print(haha[True])
